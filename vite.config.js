e83da6531e1932688dcec7185ac5b335a942441e
dmscribe-sys <dmscribe@gmail.com>
2025-11-11 23:26:27 -0500

    Final project complete: deployed to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/shoppingreact/',
+  base: '/e-plantShopping/',
   plugins: [react()],
 });
